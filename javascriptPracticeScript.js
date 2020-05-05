54414423afb89a2aeb49fcc082e84b614a8513db
Dalton Collins <dalton.collins13@ncf.edu>
2020-05-05 10:35:52 -0400

    Add files via upload

---
+++ javascriptPracticeScript.js
@@ -0,0 +1,7 @@
+
+document.getElementById("p1Rock").onclick = function () { 
+	document.getElementById("p1Display").innerHTML = "Rock";};
+
+
+document.getElementById("p2Rock").onclick = function () {
+	document.getElementById("p2Display").innerHTML = "Rock";};
